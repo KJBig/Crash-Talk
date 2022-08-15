c6f93e2b218816ce799d419412dffd6d7c80535d
kdongk <ldh5081@naver.com>
2022-08-15 18:31:52 +0900

    서버 실행

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
 const app = express();
-app.set('port', process.env.PORT || 5555);
+app.set('port', process.env.PORT || 3001);
 
 app.get('/', (req, res) => {
     res.send('서버 실행 성공!');
+});
+
+app.listen(app.get('port'), ()=> {
+    console.log(app.get('port'), '콘솔 출력');
 });
 
