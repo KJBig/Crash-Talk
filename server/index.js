86b1da264f2d9cc80d40e60779e3baad76282bbc
kdongk <ldh5081@naver.com>
2022-08-15 19:02:35 +0900

    merge check

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 3010);
 
 app.get('/', (req, res) => {
     res.send('서버 실행 성공!');
