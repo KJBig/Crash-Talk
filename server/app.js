1a12b3099709cbc2d7f419ad7032806d44411be5
KJBig <kjgi73k@gmail.com>
2022-08-21 16:04:29 +0900

    dev branch merge, client with server

---
+++ server/app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require('dotenv');
+// const fs = require('fs');
+//
+//
+// const data = fs.readFileSync('./database.json');
+// const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+
+dotenv.config();
+
+// const connection = mysql.createConnection({
+//     host: conf.host,
+//     user: conf.user,
+//     password: conf.password,
+//     port: conf.port,
+//     database: conf.database,
+// });
+
+
+const webSocket = require("./socket");
+const indexRouter = require("./routes");
+
+
+const app = express();
+app.set("port", process.env.PORT || 3001);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const connection = mysql.createConnection({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    port: process.env.port,
+    database: process.env.database,
+});
+connection.connect();
+
+app.use("/", indexRouter);
+
+
+
+// 라우터 존재 x 시
+app.use((req, res, next) => {
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+});
+
+//error 라우터
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+    res.status(err.status || 500);
+    res.render("error");
+});
+
+//리스너
+const server = app.listen(app.get("port"), () => {
+    console.log(app.get("port"), "번 포트에서 대기 중");
+});
+
+webSocket(server, app);
---
+++ server/routes/index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const path = require('path');
+const router = express.Router();
+
+const { connection } = require('../app');
+
+router.get("/", (req, res) => {
+    res.send("server is up and running");
+});
+
+router.post("/login", (req, res) => {
+    //res.send({ data: { validity: true } });
+    if (req.headers.header === "LOGIN_USER") {
+        //DB 확인
+        //DB에 겹치는게 있으면
+        res.send({data: {user: "", validity: true}});
+    }
+});
+
+router.post("/register", (req, res) => {
+    //res.send({ data: { validity: true } });
+    console.log(req.body);
+
+    if (req.headers.header === "REGISTER_USER") {
+        console.log(req.body);
+        let sql = 'INSERT INTO user VALUES (?, ?, ?, ?, ?)';
+
+        let name = req.body.name;
+        let email = req.body.email;
+        let password = req.body.password;
+        let nick_name = req.body.nickname;
+        let symbol_id = 1
+        let params = [name, email, password, nick_name, symbol_id];
+
+        connection.query(sql, params,
+            (err, rows, fields) => {
+                res.send(false);
+                console.log(err);
+                console.log(rows);
+            })
+
+        // push 성공 시, validity true 반환
+        //
+        // const data = {
+        //   validity: true,
+        // };
+        // res.send(data);
+    }
+});
+
+router.post("/home", (req, res) => {
+    if (req.headers.header === "JOIN_ROOM") {
+    }
+});
+
+router.post("/room", (req, res) => {
+    if (req.headers.header === "") {
+    }
+});
+
+
+module.exports = router;
